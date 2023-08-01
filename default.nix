d41c2e21aa263fafc066c00254d285c96780553b
William McPherson <willmcpherson2@gmail.com>
2023-08-01 13:01:43 +1000

    Output to $out/bin

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ pkgs.buildNpmPackage {
   name = "letscape";
   src = ./.;
   installPhase = ''
-    mkdir $out
-    cp -r . $out
+    mkdir -p $out/bin
+    cp -r . $out/bin
   '';
   npmDepsHash = "sha256-l5qkQa+YeAqP2kIw1VMgy7wZYxP7C/Ck9lsF17O5SHM=";
 }
